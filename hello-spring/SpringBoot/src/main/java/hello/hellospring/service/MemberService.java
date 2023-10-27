40a12651d397c50470febaeaaca50e94b1211144
onehowon <flash0221@ajou.ac.kr>
2023-10-27 11:39:47 +0900

    commit

--- hello-spring/SpringBoot/src/main/java/hello/hellospring/service/MemberService.java
+++ hello-spring/SpringBoot/src/main/java/hello/hellospring/service/MemberService.java
@@ -2,16 +2,19 @@ package hello.hellospring.service;
 
 import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
-import hello.hellospring.repository.MemoryMemberRepository;
 
 import java.util.List;
 import java.util.Optional;
 
 public class MemberService {
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
 
-//    회원가입
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    //    회원가입
 //
     public Long join(Member member){
         validateDuplicateMember(member);
