be5d3f87dbc6516fbc99eddc2b84fab767845827
onehowon <flash0221@ajou.ac.kr>
2023-10-27 11:50:50 +0900

    commit

--- hello-spring/SpringBoot/src/main/java/hello/hellospring/controller/MemberController.java
+++ hello-spring/SpringBoot/src/main/java/hello/hellospring/controller/MemberController.java
@@ -1,4 +1,14 @@
 package hello.hellospring.controller;
 
+import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class MemberController {
+
+    private final MemberService memberService;
+
+    @Autowired
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
 }
