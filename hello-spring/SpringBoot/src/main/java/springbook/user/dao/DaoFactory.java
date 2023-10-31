f5be964e6a0a87d4e5ba284a78511115a9d50cb2
onehowon <flash0221@ajou.ac.kr>
2023-10-31 15:57:24 +0900

    springboot test

--- hello-spring/SpringBoot/src/main/java/springbook/user/dao/DaoFactory.java
+++ hello-spring/SpringBoot/src/main/java/springbook/user/dao/DaoFactory.java
@@ -26,4 +26,11 @@ public class DaoFactory {
     public ConnectionMaker connectionMaker(){
         return new DConnectionMaker();
     }
+
+    DaoFactory daoFactory = new DaoFactory();
+    UserDao dao1 = factory.userDao();
+    UserDao dao2 = factory.userDao();
+
+    System.out.println(dao1);
+    System.out.println(dao2);
 }
